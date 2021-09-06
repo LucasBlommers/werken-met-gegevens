4c0efbe5d9828c6688bf686fc8533229134fea1a
lucas blommers <lucas-blommers@live.nl>
2021-09-06 10:24:51 +0200

    Pretified speelhaldag.py and feestlunch.py

--- feestlunch.py
+++ feestlunch.py
@@ -1,5 +1,9 @@
-croisant = .39
-stokbrood = 2.78
-kortingsbon = 0.5
+croisantPrijs = .39
+croisants = 17
+stokbroodPrijs = 2.78
+stokbroden = 2
+kortingsbonKorting = 0.5
+kortingsbonnen = 3
 
-print((croisant * 17) + (stokbrood * 2) - (kortingsbon * 3))
+totalPrice = (croisantPrijs * croisants) + (stokbroodPrijs * stokbroden) - (kortingsbonKorting * kortingsbonnen)
+print("De feestlunch kost je bij de bakker " + str(totalPrice) + " voor de " + str(croisants) + " croissantjes en de " + str(stokbroden) + " stokbroden als de " + str(kortingsbonnen) + " kortingsbonnen nog geldig zijn!")
--- speelhaldag.py
+++ speelhaldag.py
@@ -1,4 +1,8 @@
-ticket=7.45
+ticketPrijs=7.45
+tickets = 3
 vrPerMinuut = 0.37 / 5
+vrMinuten = 45
 
-print((3*ticket) + (vrPerMinuut * 45))
+totalPrice = (tickets*ticketPrijs) + (vrPerMinuut * vrMinuten)
+
+print("Dit geweldige dagje-uit met " + str(tickets) + " mensen in de speelhal met " + str(vrMinuten) + " minuten vr kost je maar " + str(totalPrice) + " euro")
